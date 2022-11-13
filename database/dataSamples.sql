c168395de97e39e15c64ae94f240915b8dac35ec
Laith-Grira <lgrira12@gmail.com>
2022-11-13 13:42:53 -0500

    update data samples

--- database/dataSamples.sql
+++ database/dataSamples.sql
@@ -5,7 +5,7 @@ CREATE DATABASE "uozonedb";
 -----------------------------------------
 
 CREATE TABLE "courses" (
-  "course_id" int IDENTITY(1,1) PRIMARY KEY,
+  "course_id" SERIAL PRIMARY KEY,
   "code" text NULL,
   "number" bigint NULL,
   "term" text NULL,
@@ -13,8 +13,8 @@ CREATE TABLE "courses" (
   "section" text NULL,
   "units" bigint NULL,
   "isClosed" boolean NULL,
-  "name.en" text NULL,
-  "name.fr" text NULL,
+  "name_en" text NULL,
+  "name_fr" text NULL,
   "startDate" text NULL,
   "endDate" text NULL,
   "classes" json NULL
@@ -23,7 +23,7 @@ CREATE TABLE "courses" (
 -----------------------------------------
 
 INSERT INTO "courses" VALUES
-('CHM',1311,'fall',2022,'A',3,FALSE,'Principles of Chemistry','Principes de chimie','2022-09-07','2022-12-07','[{"type":"LEC","sectionCode":"00","instructor":"Alain St-Amant","day":{"en":"Monday","fr":"Lundi"},"startTime":"5:30PM","endTime":"6:50PM","location":{"address":"100 Louis Pasteur","department":"CRX","room":"C140"},"seats":{"capacity":300,"taken":235,"seatsSEG":90}},{"type":"LEC","sectionCode":"00","instructor":"Alain St-Amant","day":{"en":"Wednesday","fr":"Mercredi"},"startTime":"5:30PM","endTime":"6:50PM","location":{"address":"100 Louis Pasteur","department":"CRX","room":"C140"},"seats":{"capacity":300,"taken":235,"seatsSEG":90}},{"type":"DGD","sectionCode":"01","instructor":"Staff","day":{"en":"Wednesday","fr":"Mercredi"},"startTime":"4:00PM","endTime":"5:20PM","location":{"address":"65 University","department":"MRT","room":"218"},"seats":{"capacity":100,"taken":30,"seatsSEG":20}},{"type":"DGD","sectionCode":"02","instructor":"Staff","day":{"en":"Wednesday","fr":"Mercredi"},"startTime":"5:30PM","endTime":"6:50PM","location":{"address":"65 University","department":"MRT","room":"217"},"seats":{"capacity":100,"taken":30,"seatsSEG":20}},{"type":"LAB","sectionCode":"01","instructor":"Staff","day":{"en":"Friday","fr":"Vendredi"},"startTime":"4:00PM","endTime":"5:20PM","location":{"address":"65 University","department":"MRT","room":"318"},"seats":{"capacity":100,"taken":30,"seatsSEG":20}},{"type":"LAB","sectionCode":"02","instructor":"Staff","day":{"en":"Thursday","fr":"Jeudi"},"startTime":"10:00AM","endTime":"11:20AM","location":{"address":"65 University","department":"MRT","room":"318"},"seats":{"capacity":100,"taken":30,"seatsSEG":20}}]');
+(1, 'CHM',1311,'fall',2022,'A',3,FALSE,'Principles of Chemistry','Principes de chimie','2022-09-07','2022-12-07','[{"type":"LEC","sectionCode":"00","instructor":"Alain St-Amant","day":{"en":"Monday","fr":"Lundi"},"startTime":"5:30PM","endTime":"6:50PM","location":{"address":"100 Louis Pasteur","department":"CRX","room":"C140"},"seats":{"capacity":300,"taken":235,"seatsSEG":90}},{"type":"LEC","sectionCode":"00","instructor":"Alain St-Amant","day":{"en":"Wednesday","fr":"Mercredi"},"startTime":"5:30PM","endTime":"6:50PM","location":{"address":"100 Louis Pasteur","department":"CRX","room":"C140"},"seats":{"capacity":300,"taken":235,"seatsSEG":90}},{"type":"DGD","sectionCode":"01","instructor":"Staff","day":{"en":"Wednesday","fr":"Mercredi"},"startTime":"4:00PM","endTime":"5:20PM","location":{"address":"65 University","department":"MRT","room":"218"},"seats":{"capacity":100,"taken":30,"seatsSEG":20}},{"type":"DGD","sectionCode":"02","instructor":"Staff","day":{"en":"Wednesday","fr":"Mercredi"},"startTime":"5:30PM","endTime":"6:50PM","location":{"address":"65 University","department":"MRT","room":"217"},"seats":{"capacity":100,"taken":30,"seatsSEG":20}},{"type":"LAB","sectionCode":"01","instructor":"Staff","day":{"en":"Friday","fr":"Vendredi"},"startTime":"4:00PM","endTime":"5:20PM","location":{"address":"65 University","department":"MRT","room":"318"},"seats":{"capacity":100,"taken":30,"seatsSEG":20}},{"type":"LAB","sectionCode":"02","instructor":"Staff","day":{"en":"Thursday","fr":"Jeudi"},"startTime":"10:00AM","endTime":"11:20AM","location":{"address":"65 University","department":"MRT","room":"318"},"seats":{"capacity":100,"taken":30,"seatsSEG":20}}]');
 
 
 INSERT INTO "courses" VALUES
@@ -33,7 +33,7 @@ INSERT INTO "courses" VALUES
 -----------------------------------------
 
 CREATE TABLE "cart" (
-  "cart_item_id" int IDENTITY(1,1) PRIMARY KEY,
+  "cart_item_id" SERIAL PRIMARY KEY,
   "code" text NULL,
   "number" bigint NULL,
   "term" text NULL,
@@ -41,8 +41,8 @@ CREATE TABLE "cart" (
   "section" text NULL,
   "units" bigint NULL,
   "isClosed" boolean NULL,
-  "name.en" text NULL,
-  "name.fr" text NULL,
+  "name_en" text NULL,
+  "name_fr" text NULL,
   "startDate" text NULL,
   "endDate" text NULL,
   "classes" json NULL
